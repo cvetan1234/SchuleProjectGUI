c68a9288d3d4b3feb2de510905686a8b5dc66837
cvetan1234 <72192783+cvetan1234@users.noreply.github.com>
2021-01-24 19:54:18 +0200

    Functionality to do list -> DONE

--- Translator.java
+++ Translator.java
@@ -5,9 +5,12 @@ import java.awt.event.*;
 import java.util.concurrent.*; 
 
 public class Translator extends Sound {
-	static String[] morse = {".- ", "-... ",".-- ","--. ","-.. ",". ","...- ","--.. ",".. ",".--- ","-.- ",".-.. ","-- ","-. ","--- ",".--. ",".-. ","... ","- ","..- ","..-. ",".... ","-.-. ","---. ","---- ","--.- ","-..- ","-.-- ","..-- ",".-.- ","----- ",".---- ","..--- ","...-- ","....- ","..... ","-.... ","--... ","---.. ","----. ",".-.-.- ","--..-- ","..--.. ","-.-.-- ", "---... ","-.--. ","-.--.- ", "-....- ", "/ "};
-	static String[] letters = {"а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ь", "ю", "я", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", ":", "(", ")", "-", " "};
+	static String[] morse = {"", ".- ", "-... ",".-- ","--. ","-.. ",". ","...- ","--.. ",".. ",".--- ","-.- ",".-.. ","-- ","-. ","--- ",".--. ",".-. ","... ","- ","..- ","..-. ",".... ","-.-. ","---. ","---- ","--.- ","-..- ","-.-- ","..-- ",".-.- ","----- ",".---- ","..--- ","...-- ","....- ","..... ","-.... ","--... ","---.. ","----. ",".-.-.- ","--..-- ","..--.. ","-.-.-- ", "---... ","-.--. ","-.--.- ", "-....- ", "/ "};
+	static String[] letters = {"", "а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ь", "ю", "я", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", ":", "(", ")", "-", " "};
     static TimeUnit time = TimeUnit.MILLISECONDS;
+    static boolean reversedOrNotReversed = true;
+    static boolean isTheInformationCorrect = true;
+    static String text;
     
 	public static void main(String[] args) {
 		JFrame f = new JFrame("Translator");
@@ -18,25 +21,57 @@ public class Translator extends Sound {
 		l.setOpaque(true);
 		l.setBackground(Color.yellow);
 		
+		JLabel reversed = new JLabel ("Letters to morse code", SwingConstants.CENTER);
+		reversed.setBounds(0,12,200,100);
+		
 		JTextArea t = new JTextArea("Пример"); //creating instance of JTextArea
 		t.setBounds(20, 70, 270, 340); // set x axis, y axis, width, height  of the JTextArea
 		
-		JTextArea t1 = new JTextArea(".- .-. .. -- . .-. "); //creating instance of JTextArea
+		JTextArea t1 = new JTextArea(".--. .-. .. -- . .-. "); //creating instance of JTextArea
 		t1.setBounds(310, 70, 270, 340); // set x axis, y axis, width, height  of the JTextArea
 		
 		JButton translate = new JButton("Translate"); //creating instance of JButton
 		translate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){  
-	            t1.setText(sentence(t.getText()));  
+			public void actionPerformed(ActionEvent e){ 
+				if (reversedOrNotReversed) {
+					text = sentence(t.getText());
+					if (isTheInformationCorrect) {
+						t1.setText(text);
+					}else {
+						t1.setText("Wrong input.");
+					}
+				} else {
+					text = sentenceReversed(t.getText());
+					if (isTheInformationCorrect) {
+						t1.setText(text);
+					}else {
+						t1.setText("Wrong input.");
+					}
+				}
+				
 			}  
 		});
 		JButton read = new JButton("read"); //creating instance of JButton 
 		read.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){  
-				sound(t1.getText());
+				if (reversedOrNotReversed) {
+					sound(t1.getText());
+				}else {
+					sound(t.getText());
+				}
 			}  
 		});
 		JButton reverse = new JButton("R"); //creating instance of JButton 
+		reverse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+				reversedOrNotReversed = !reversedOrNotReversed;
+				if (reversedOrNotReversed) {
+					reversed.setText("Letters to morse code");
+				} else {
+					reversed.setText("Morse code to letters");
+				}
+			}  
+		});
 	    JButton quit = new JButton("X"); //creating instance of JButton 
 	    quit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){  
@@ -51,9 +86,10 @@ public class Translator extends Sound {
 		
 		f.add(translate); //add the elements to the Frame
 		f.add(read);
-		f.add(reverse);
+		f.add(reversed);
 		f.add(quit);
 		f.add(l);
+		f.add(reverse);
 		f.add(t);
 		f.add(t1);
 		f.setLayout(null); // using no layout managers
@@ -62,15 +98,36 @@ public class Translator extends Sound {
 	
 	static String tr (String s) {
 		int position = 0;
-		for (int n = 0; n<49; n++) {
+		for (int n = 0; n<50; n++) {
 			if (s.equals(letters[n])){
 				position = n; break;
 			}
 		}
+		if (position == 0) {
+			isTheInformationCorrect = false;
+		} else {
+			isTheInformationCorrect = true;
+		}
 		return morse[position];
 	}
 	
+	static String trReversed (String s) {
+		int position = 0;
+		for (int n = 0; n<50; n++) {
+			if (s.equals(morse[n])){
+				position = n; break;
+			}
+		}
+		if (position == 0) {
+			isTheInformationCorrect = false;
+		} else {
+			isTheInformationCorrect = true;
+		}
+		return letters[position];
+	}
+	
 	static String sentence (String s) {
+		s = s.toLowerCase();
 		String a = "";
 		int length = s.length();
 		char ch;
@@ -86,6 +143,29 @@ public class Translator extends Sound {
 		return a;
 	}
 	
+	static String sentenceReversed (String s) {
+		String a = "";
+		String symbol = "";
+		String letter = "";
+		char ch = s.charAt(0);
+		int length = s.length();
+		if (s.charAt(length-1) != ' ') {
+			s += ' ';
+		}
+		for (int n = 0; n<length; n++) {
+			ch = s.charAt(n);
+			while (ch != ' ') {
+				letter = String.valueOf(ch);
+				symbol += letter;
+				n++;
+				ch = s.charAt(n);
+			} 
+			symbol += " ";
+			a += trReversed(symbol);
+			symbol = "";
+		}
+		return a;
+	}
 	public static void sleep (long seconds) {
     	try { 
     		  time.sleep(seconds);  
@@ -103,16 +183,15 @@ public class Translator extends Sound {
 			ch = s.charAt(n);
 			if (ch == '.') {
 				play("/333_1+.wav");
-			    sleep(333);
+				sleep(500);
 			}if (ch == '-') {
 				play("/333_3+.wav");
-				sleep(333);
+				sleep(1500);
 			}else if (ch == ' ') {
-				sleep(1000);
+				sleep(500);
 			}else if (ch == '/') {
-				sleep(333);
+				sleep(1500);
 			}
 		}
 	}
-
 }
